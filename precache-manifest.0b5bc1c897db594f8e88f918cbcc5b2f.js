2f094fed7feeecb09c17cf1af613ad330cfdec56
Kwangsoo Jeon <evan.ksjeon@gmail.com>
2019-06-08 22:36:49 +1200

    Updates

---
+++ precache-manifest.0b5bc1c897db594f8e88f918cbcc5b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965c2d0b872b0379487a94012b81114e",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "59759d943250ce09722b",
+    "url": "/Movie_app/static/css/main.f0faf8ab.chunk.css"
+  },
+  {
+    "revision": "82570cd7efab72a4b435",
+    "url": "/Movie_app/static/js/2.e93332a3.chunk.js"
+  },
+  {
+    "revision": "59759d943250ce09722b",
+    "url": "/Movie_app/static/js/main.d48a9247.chunk.js"
+  },
+  {
+    "revision": "bc3a21fec6a0d0ce35f7",
+    "url": "/Movie_app/static/js/runtime~main.bdf1a524.js"
+  }
+]);
